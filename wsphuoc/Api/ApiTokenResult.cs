83e691c7c777cb76925b157d66b423f3b50bdcb3
Phan Huu Phuoc <wsphuoc@gmail.com>
2021-08-09 15:54:07 +0700

    Thêm ApiTokenResult

---
+++ wsphuoc/Api/ApiTokenResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using wsphuoc.Exceptions;
+
+namespace wsphuoc.Api
+{
+    public class ApiTokenResult:ApiResult
+    {
+        public string Token { get; set; }
+        public ApiTokenResult()
+        {
+            throw new wsphuocException("Error token");
+        }
+        public ApiTokenResult(string token)
+        {
+            IsSuccessed = true;
+            Token = token;
+        }
+        public ApiTokenResult(string token, string message)
+        {
+            IsSuccessed = true;
+            Token = token;
+            Message = message;
+        }
+    }
+}
