aeddf33a2fb4984fb32ce883c5d4717f8bc8bb1a
Phan Huu Phuoc <wsphuoc@gmail.com>
2021-08-09 13:59:22 +0700

    Thêm wsphuocException:Exception

---
+++ wsphuoc/Exceptions/Exception.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wsphuoc.Exceptions
+{
+    public class wsphuocException : Exception
+    {
+        public wsphuocException() { }
+        public wsphuocException(string message)
+            : base(message)
+        {
+
+        }
+        public wsphuocException(string message, Exception inner)
+            :base(message, inner)
+        {
+
+        }
+    }
+}
